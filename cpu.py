b2ee7e4425cd4464d985a9a293c68bc726f863b8
Paul Crowley <paul@ciphergoth.org>
2009-08-03 20:05:07 +0100

    Move timing into CPU object

--- cpu.py
+++ cpu.py
@@ -6,6 +6,8 @@ import wx
 class CPU(object):
     def __init__(self, partner):
         self._partner = partner
+        self._traceProxy = self.NonblockingProxyFunction(self._trace)
+        self._lineTime = 1000
         
     def NonblockingProxyFunction(self, f):
         def callF(*a, **kw):
@@ -28,7 +30,6 @@ class CPU(object):
     def Play(self):
         self._globals = self._partner.getGlobals()
         self._program = self._partner.program()
-        self._trace = self.NonblockingProxyFunction(self._partner.traceLine)
         t = threading.Thread(target = self._start)
         t.start()
     
@@ -47,9 +48,21 @@ class CPU(object):
         #print frame, event, arg, frame.f_code.co_filename, frame.f_lineno
         if "<string>" in frame.f_code.co_filename:
             if event == "line":
-                self._trace(frame.f_lineno)
+                self._traceProxy(frame.f_lineno)
         return self._tracefunc
+
+    def _trace(self, rcb, lineno):
+        self._partner.traceLine(lineno)
+        self._rcb = rcb
+        self._timer = wx.CallLater(self._lineTime, self._release)
+    
+    def _release(self):
+        r = self._rcb
+        self._rcb = None
+        self._timer = None
+        r((None, None))
     
     def _done(self, e):
+        self._partner.traceLine(None)
         self._partner.done(e)
 
