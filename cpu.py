b290256882df60e413b2b81a1a217eae3251debd
Paul Crowley <paul@lshift.net>
2009-08-03 17:42:55 +0100

    Four items off the todo list

--- cpu.py
+++ cpu.py
@@ -1,38 +1,61 @@
 import sys
-import thread
+import threading
 import Queue
 import wx
-
-def proxyFunction(f):
-    def callF(*a, **kw):
-        q = Queue.Queue()
-        wx.CallAfter(f, q, *a, **kw)
-        return q.get()
-    return callF
 
 class CPU(object):
     def __init__(self):
         self._functions = {}
         
+    def NonblockingProxyFunction(self, f):
+        def callF(*a, **kw):
+            q = Queue.Queue()
+            wx.CallAfter(f, q.put, *a, **kw)
+            res, exc = q.get()
+            if exc is not None:
+                raise exc
+            return res
+        return callF
+
+    def ProxyFunction(self, f):
+        def callBlocking(rcb, *a, **kw):
+            try:
+                rcb((f(*a, **kw), None))
+            except Exception, e:
+                rcb((None, e))
+        return self.NonblockingProxyFunction(callBlocking)
+
     def SetTrace(self, f):
-        self._trace = proxyFunction(f)
+        self._trace = self.NonblockingProxyFunction(f)
     
     def SetProgram(self, p):
         self._program = p
 
     def AddFunction(self, name, f):
-        self._functions[name] = proxyFunction(f)
+        self._functions[name] = self.ProxyFunction(f)
     
     def Start(self):
-        thread.start_new_thread(self._start, tuple([]))
+        t = threading.Thread(target = self._start)
+        t.start()
     
     def _start(self):
-        sys.settrace(self._tracefunc)
-        exec self._program in self._functions
-        sys.settrace(None)
+        try:
+            sys.settrace(self._tracefunc)
+            try:
+                exec self._program in self._functions
+                wx.CallAfter(self._done, None)
+            except Exception, e:
+                wx.CallAfter(self._done, e)
+        finally:
+            sys.settrace(None)
     
     def _tracefunc(self, frame, event, arg):
         #print frame, event, arg, frame.f_code.co_filename, frame.f_lineno
-        if event == "line" and "<string>" in frame.f_code.co_filename:
-            self._trace(frame.f_lineno)
+        if "<string>" in frame.f_code.co_filename:
+            if event == "line":
+                self._trace(frame.f_lineno)
         return self._tracefunc
+            
+    def _done(self, e):
+        print "Done, exception:", e
+
